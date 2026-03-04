47f6a15f7d79c8ce23433fc18aeb3237a3e6271a
AlunoADS1 <siqsss206@gmail.com>
2026-03-04 16:50:44 -0300

    feat: Opções de encerramento da calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,30 +1,44 @@
  function calculadora(){
     while (true) {
-    let resultado=0.0;
-    let operando1
-    let operando2
-    let operador
-    operando1 = parseFloat(prompt("Digite o primeiro número: "))
-    operando2 = parseFloat(prompt("Digite o segundo número: "))
-    operador = (prompt (":D digite uma das operações (+ - * /):"))
+        let resultado=0.0;
+        let operando1;
+        let operando2;
+        let operador;
+        let continua;  
+        let operador_valido = true;
 
-        if (operador==="+"){
-            resultado = operando1 + operando2
-        } else if (operador === "-"){
-            resultado = operando1 - operando2
-        }else if (operador==="*"){
-            resultado=operando1*operando2
-        }else if (operador==="/"){
-            if (operando2==0){
-                alert("impossível dividir por 0")
-                return
-            resultado= operando1/operando2
+        operando1 = parseFloat(prompt("Digite o primeiro número: "));
+        operando2 = parseFloat(prompt("Digite o segundo número: "));
+        operador = prompt ("Digite uma das operações (+ - * /):");
+
+            if (operador==="+"){
+                resultado = operando1 + operando2;
+            } else if (operador === "-"){
+                resultado = operando1 - operando2;
+            }else if (operador==="*"){
+                resultado = operando1 * operando2;
+            }else if(operador==="/"){
+                if(operando2==0){
+                    alert("impossível dividir por 0");
+                    continua = prompt("Digite sim para continuar e não para encerrar");
+                if(continua === "não"){
+                    return
+                } 
+            } else {
+                resultado = operando1 / operando2;
             }
-        }else {
-            alert("): Operador inválido!")
-            return
+        } else {
+          
+            alert("Operador inválido!");
+            operador_valido = falso
+            continua = prompt("Digite sim para continuar e não para encerrar");
+            if(continua === "não"){
+            return 
         }
-    
-alert("Resultado: "+ operando1+ " " +operador+operando2+" = "+resultado)
-
+        if(operador != "/" || operando2 != 0){
+            alert("Resultado: " + operando1 + " " + operador + " " + operando2 + " = " + resultado);
+            
+        }
+            
+    }
 }
